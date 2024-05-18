fae5b60d9613918910a0fa3ed503981ccc3e724d
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2024-05-18 22:50:07 +0700

    feat: add app methods

--- src/lib/app.py
+++ src/lib/app.py
@@ -1,10 +1,28 @@
 class App:
   def __init__(self):
     # Constructor code here
-    pass
+    self.data = {}
 
   def run(self):
     # Method code here
+    pass
+
+  def ping(self):
+    pass
+
+  def get(self, key):
+    pass
+
+  def set(self, key, value):
+    pass
+
+  def strln(self, key):
+    pass
+
+  def delete(self, key):
+    pass
+
+  def append(self, key):
     pass
 
   def shutdown(self):

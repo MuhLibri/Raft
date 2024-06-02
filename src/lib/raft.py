8b77737a16c12c11c5ad6033d698f8c094fbb34a
NerbFox <13521043@std.stei.itb.ac.id>
2024-06-02 17:31:25 +0700

    feat: heartbeat leader and follower
    Co-authored-by: M Farrel Danendra Rachim <Breezy-DR@users.noreply.github.com>
    Co-authored-by: Muhammad Equilibrie Fajria <MuhLibri@users.noreply.github.com>
    Co-authored-by: Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>

--- src/lib/raft.py
+++ src/lib/raft.py
@@ -1,14 +1,12 @@
 import asyncio
 import json
 import socket
-from threading     import Thread
 import time
+from threading     import Thread
 from xmlrpc.client import ServerProxy
 from typing        import Any, List
 from enum          import Enum
 from .struct       import Address
-
-
 
 class RaftNode:
     HEARTBEAT_INTERVAL   = 1
@@ -21,6 +19,7 @@ class RaftNode:
         CANDIDATE = 2
         FOLLOWER  = 3
 
+    # Public Raft Node methods
     def __init__(self, application : Any, addr: Address, contact_addr: Address = None):
         socket.setdefaulttimeout(RaftNode.RPC_TIMEOUT)
         self.address:             Address           = addr
@@ -33,10 +32,9 @@ class RaftNode:
         if contact_addr is None:
             self.cluster_addr_list.append(self.address)
             self.__initialize_as_leader()
+            # self.start_election()
         else:
             self.__try_to_apply_membership(contact_addr)
-
-
 
     # Internal Raft Node methods
     def __print_log(self, text: str):
@@ -63,10 +61,17 @@ class RaftNode:
             self.__print_log("[Leader] Sending heartbeat...")
             for node_addr in self.cluster_addr_list:
                 if node_addr != self.address:
-                    self.__send_request({"heartbeat": True}, "heartbeat", node_addr)
+                    self.__print_log(f"[Leader] Sending heartbeat to {node_addr}")
+                    self.__send_request({
+                        "address": self.address,
+                        "heartbeat": True
+                        }, "ping", node_addr)
             await asyncio.sleep(RaftNode.HEARTBEAT_INTERVAL)
 
+
+    
     def __try_to_apply_membership(self, contact_addr: Address):
+        self.__print_log(f"Trying to apply membership to {contact_addr}")
         redirected_addr = contact_addr
         response = {
             "status": "redirected",
@@ -76,6 +81,7 @@ class RaftNode:
             }
         }
         while response["status"] != "success":
+            self.__print_log(f"Redirected to {redirected_addr}")
             redirected_addr = Address(response["address"]["ip"], response["address"]["port"])
             try:
                 response        = self.__send_request(self.address, "apply_membership", redirected_addr)
@@ -84,18 +90,38 @@ class RaftNode:
         self.log                 = response["log"]
         self.cluster_addr_list   = response["cluster_addr_list"]
         self.cluster_leader_addr = redirected_addr
+        self.__print_log(f"Membership applied successfully. Cluster Leader: {self.cluster_leader_addr}")
+        # # nyalain heartbeat
+        # self.heartbeat_thread = Thread(target=asyncio.run,args=[self.__follower_heartbeat()])
+        # self.heartbeat_thread.start()
 
     def __send_request(self, request: Any, rpc_name: str, addr: Address) -> "json":
         # Warning : This method is blocking
+        # try:
         node         = ServerProxy(f"http://{addr.ip}:{addr.port}")
         json_request = json.dumps(request)
         rpc_function = getattr(node, rpc_name)
+        self.__print_log(f"JSON Request: {json_request}")
         response     = json.loads(rpc_function(json_request))
         self.__print_log(f"Request: {request}")
         self.__print_log(f"Response: {response}")
         return response
+        # except Exception as e:   
+            # self.__print_log(f"Error: {e}")
+            # return None
 
     def start_election(self):
+        # timeout for election
+        election_timeout = time.time() + RaftNode.ELECTION_TIMEOUT_MAX
+        while time.time() < election_timeout:
+            if self.type == RaftNode.NodeType.LEADER:
+                return
+            elif self.type == RaftNode.NodeType.FOLLOWER:
+                return
+            elif self.type == RaftNode.NodeType.CANDIDATE:
+                self.__start_election_round()
+                
+    def __start_election_round(self):
         self.election_term += 1
         self.type = RaftNode.NodeType.CANDIDATE
         self.votes_received = 1  # Vote for self
@@ -114,7 +140,7 @@ class RaftNode:
     def heartbeat(self, json_request: str) -> "json":
         # TODO : Implement heartbeat
         request = json.loads(json_request)
-        self.__print_log(f"[Follower] Received heartbeat from {request['address']}")
+        self.__print_log(f"Received heartbeat from {request['address']}")
         response = {
             "heartbeat_response": "ack", 
             "address": {
@@ -122,6 +148,17 @@ class RaftNode:
                 "port": self.address.port}}
         return json.dumps(response)
 
+    def ping(self, json_request: str) -> "json":
+        request = json.loads(json_request)
+        self.__print_log(f"Received ping from {request['address']}")
+        response = {
+            "status": "success", 
+            "ping_response": "pong",
+            "address": {
+                "ip": self.address.ip, 
+                "port": self.address.port}}
+        return json.dumps(response)
+    
     def request_vote(self, json_request: str) -> "json":
         request = json.loads(json_request)
         vote_granted = False
@@ -145,8 +182,11 @@ class RaftNode:
         return json.dumps(response)
     
     def apply_membership(self, json_request: str) -> "json":
+        self.__print_log(f"Received apply_membership request from {json_request}")
+        self.__print_log(f"Current cluster_addr_list: {self.cluster_addr_list}")
         request = json.loads(json_request)
         self.cluster_addr_list.append(Address(request["ip"], request["port"]))
+        self.__print_log(f"Updated cluster_addr_list: {self.cluster_addr_list}")
         response = {
             "status": "success", 
             "log": self.log, 

95529df3851a877d4c8163987c7e2e49874f987b
Nigel Sahl <93074692+NerbFox@users.noreply.github.com>
2024-06-03 13:47:40 +0700

    feat: Fix Address class equality comparison

--- src/lib/struct/address.py
+++ src/lib/struct/address.py
@@ -1,14 +1,10 @@
 from typing import List
 class Address(dict):
-    # create static list of addresses
-    addresses = []
     def __init__(self, ip: str, port: int):
         dict.__init__(self, ip=ip, port=port)
         self.ip   = ip
         self.port = port
-        # if self not in Address.addresses:
-        Address.addresses.append(self)
-                
+                          
     def __str__(self):
         return f"{self.ip}:{self.port}"
     
@@ -16,7 +12,7 @@ class Address(dict):
         return iter((self.ip, self.port))
     
     def __eq__(self, other):
-        return self.ip == other.ip and self.port == other.port
+        return self['ip'] == other['ip'] and self['port'] == other['port']
     
     def __ne__(self, other):
-        return self.ip != other.ip or self.port != other.port
+        return not self.__eq__(other)

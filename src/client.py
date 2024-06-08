733af3705c7aefc1326d6ec948bbb8e89e1835a9
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2024-06-08 13:49:57 +0700

    feat: client execute

--- src/client.py
+++ src/client.py
@@ -3,7 +3,6 @@ from xmlrpc.client import ServerProxy
 from lib.app import App
 from lib.struct import Address
 from lib.struct import KVStore
-from lib.key_value_services import KeyValueService
 
 def shutdown():
     print("Shutting down...")
@@ -15,14 +14,8 @@ if __name__ == "__main__":
         exit()
 
     server_addr = Address(sys.argv[1], int(sys.argv[2]))
-    # with ServerProxy(f"http://{server_addr.ip}:{server_addr.port}") as server:
-    #     store = KVStore()
-    #     app = App(store, server, server_addr)
-    #     app.run()
-    
-    server = ServerProxy(f"http://{server_addr.ip}:{server_addr.port}")
-    store = KVStore()
-    service = KeyValueService(store, server_addr)
+    with ServerProxy(f"http://{server_addr.ip}:{server_addr.port}") as server:
+        app = App(KVStore(), server, server_addr)
     
     while True:
         command = input("Enter command: ").strip().split()
@@ -33,20 +26,21 @@ if __name__ == "__main__":
         args = command[1:]
 
         if cmd == "ping":
-            print(service.ping())
+            app.ping()
         elif cmd == "get" and len(args) == 1:
-            print(service.get(args[0]))
+            app.get(args[0])
         elif cmd == "set" and len(args) == 2:
-            print(service.set(args[0], args[1]))
+            app.set(args[0], str(args[1]))
         elif cmd == "strln" and len(args) == 1:
-            print(service.strln(args[0]))
-        elif cmd == "delete" and len(args) == 1:
-            print(service.delete(args[0]))
+            app.strln(args[0])
+        elif cmd == "del" and len(args) == 1:
+            app.delete(args[0])
         elif cmd == "append" and len(args) == 2:
-            print(service.append(args[0], args[1]))
+            app.append(args[0], str(args[1]))
+        elif cmd == "append" and len(args) == 1:
+            app.append(args[0], None)
         elif cmd == "shutdown":
             shutdown()
             break
         else:
-            print("Invalid command or arguments")
-    
+            print("Invalid command or arguments\n")

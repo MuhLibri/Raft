e7978cb3826482764ee31d50c57415a79d45b636
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-06-09 23:01:00 +0700

    fix: time out

--- client/web/static/js/main.js
+++ client/web/static/js/main.js
@@ -1,4 +1,6 @@
 /* Input text value */
+const key = document.getElementById('key');
+const value = document.getElementById('value');
 
 /* Input */
 const commandSelect = document.getElementById('command');
@@ -9,7 +11,6 @@ const serverOutput = document.getElementById('server-info-out');
 const commandOutput = document.getElementById('command-info-out');
 const resultOutput = document.getElementById('result-info-out');
 
-
 /* UI */
 const loadingSpinner = document.getElementById('loading-spinner');
 const valueGroup = document.getElementById('value-group');
@@ -17,9 +18,6 @@ const keyGroup = document.getElementById('key-group');
 
 function sendCommand() {
     loadingSpinner.style.display = 'block';  // Show spinner before sending the request
-    const key = document.getElementById('key').value;
-    const value = document.getElementById('value').value;
-
 
     let url = '';
     let method = 'GET';
@@ -28,46 +26,55 @@ function sendCommand() {
     if (commandSelect.value === 'ping') {
         url = '/ping';
     } else if (commandSelect.value === 'get') {
-        url = `/get/${key}`;
+        url = `/get/${key.value}`;
     } else if (commandSelect.value === 'set') {
         url = '/set';
         method = 'POST';
-        body = JSON.stringify({ key: key, value: value });
+        body = JSON.stringify({ key: key.value, value: value.value });
     } else if (commandSelect.value === 'delete') {
-        url = `/delete/${key}`;
+        url = `/delete/${key.value}`;
         method = 'DELETE';
     } else if (commandSelect.value === 'append') {
         url = '/append';
         method = 'POST';
-        body = JSON.stringify({ key: key, value: value });
-        console.log(body);
+        body = JSON.stringify({ key: key.value, value: value.value });
     }
 
     console.log(`Sending ${method} request to ${url} with body: ${body}`);
+
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 10000);  // 10 sec
 
     fetch(url, {
         method: method,
         headers: {
             'Content-Type': 'application/json'
         },
-        body: method !== 'GET' ? body : null
+        body: method !== 'GET' ? body : null,
+        signal: controller.signal
     })
-    .then(response => response.json())
     .then(response => {
-        loadingSpinner.style.display = 'none';  // Hide spinner on success
-        resultOutput.innerText = JSON.stringify(response, null, 1);
-        serverOutput.innerText = `${response.server_ip}:${response.server_port}`;
-        commandOutput.innerText = build_command_output(commandSelect.value, key, value);
+        clearTimeout(timeoutId); 
+        return response.json();
+    })
+    .then(response => {
+        loadingSpinner.style.display = 'none'; 
+        resultOutput.innerText = JSON.stringify(response, null, 2);
+        serverOutput.innerText = `${response.server_ip?? ''}:${response.server_port?? ''}`;
+        commandOutput.innerText = build_command_output(commandSelect.value, key.value, value.value);
     })
     .catch(error => {
-        loadingSpinner.style.display = 'none';  // Hide spinner on error
-        resultOutput.innerText = `Error: ${error}`;
+        loadingSpinner.style.display = 'none'; 
+        if (error.name === 'AbortError') {
+            resultOutput.innerText = 'Error: Request timed out';
+        } else {
+            resultOutput.innerText = `Error: ${error}`;
+        }
         serverOutput.innerText = '';
-        commandOutput.innerText = build_command_output(commandSelect.value, key, value);
     });
 }
 
-function build_command_output(command, key=null, value=null) {
+function build_command_output(command, key = null, value = null) {
     let output = command;
     if (key) {
         output += ` ${key}`;
@@ -76,11 +83,9 @@ function build_command_output(command, key=null, value=null) {
         output += ` ${value}`;
     }
     return output;
-
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    
     commandSelect.addEventListener('change', function() {
         if (this.value === 'ping') {
             keyGroup.style.display = 'none';
@@ -90,12 +95,6 @@ document.addEventListener('DOMContentLoaded', function() {
             valueGroup.style.display = 'none';
         } else {
             keyGroup.style.display = 'block';
-            valueGroup.style.display = 'block';
-        }
-
-        if (this.value === 'ping' || this.value === 'get' || this.value === 'delete') {
-            valueGroup.style.display = 'none';
-        } else {
             valueGroup.style.display = 'block';
         }
     });

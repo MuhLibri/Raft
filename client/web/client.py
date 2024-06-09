010688cfbc198898484ed21c838c33b99182218d
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-06-09 18:46:19 +0700

    feat: client api

---
+++ client/web/client.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify, render_template
+import json
+import xmlrpc.client
+from lib.app import App
+from lib.struct import Address
+
+app = Flask(__name__)
+
+server_addr = Address("localhost", 8000) 
+server = xmlrpc.client.ServerProxy(f"http://{server_addr.ip}:{server_addr.port}")
+rpc_app = App(server, server_addr)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    response = rpc_app.ping()
+    print("Ping response:", response)
+    return jsonify(response)
+
+@app.route('/get/<key>', methods=['GET'])
+def get_value(key):
+    response = rpc_app.get(key)
+    return jsonify(response)
+
+@app.route('/set', methods=['POST'])
+def set_value():
+    data = request.json
+    key = data.get('key')
+    value = data.get('value')
+    response = rpc_app.set(key, value)
+    return jsonify(response)
+
+@app.route('/delete/<key>', methods=['DELETE'])
+def delete_value(key):
+    response = rpc_app.delete(key)
+    return jsonify(response)
+
+@app.route('/append', methods=['POST'])
+def append_value():
+    data = request.json
+    key = data.get('key')
+    value = data.get('value')
+    response = rpc_app.append(key, value)
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
